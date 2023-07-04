2432a984129af0c4e8332b7d15a8e1239541aa99
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-07-04 18:45:53 +0200

    first commit: Load initial file

---
+++ VariadicParameters.playground/Pages/DebugPrint.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+/*:
+ [< Previous](@previous)                    [Home](Introduction)
+ ## Debug Print
+ - Callout(Debug Print): Create a new print function that extends the usefulness of the variadic print function.
+*/
+import Foundation
+
+code(for: "Namespace Debug print") {
+    // Recreate the print function in an Enum namespace
+}
+
+code(for: "Emoji previx") {
+    // add a number of emooji cases to the enum and use as a prefix for the print
+}
+
+code(for: "Debug Identifiers") {
+    // Extend the function to allow for an optional set of Swift Keyword debug identifiers
+}
+/*:
+ [< Previous](@previous)                    [Home](Introduction)
+*/
---
+++ VariadicParameters.playground/Pages/TheBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ [< Previous](@previous)                    [Home](Introduction)                    [Next >](@next)
+ ## Variadic Parameters
+ - Callout(Variadic Parameters): Variadic parameters in Swift allow you to accept an indefinite number of arguments of the same type within a function.
+*/
+import Foundation
+
+code(for: "Without Variadic Parameters") {
+    // Create a function that accepts an array of ints and returns the sum
+    
+    // Print the sum of an array of numbers
+
+}
+code(for: "With Variadic Parmeters") {
+    // Change the parameter to a variadic parameter
+    
+}
+
+code(for: "String Concatenation") {
+    // create a variadic function that will combine strings and separate them with a given separator
+    
+}
+/*:
+ [< Previous](@previous)                    [Home](Introduction)                    [Next >](@next)
+*/
