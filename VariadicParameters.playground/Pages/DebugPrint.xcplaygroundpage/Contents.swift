18d064919b5a127b40651022688d227d5e5dc5e4
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-07-07 01:57:52 +0200

    feat: Debug Print function (completed Swift Variadic Parameters)

--- VariadicParameters.playground/Pages/DebugPrint.xcplaygroundpage/Contents.swift
+++ VariadicParameters.playground/Pages/DebugPrint.xcplaygroundpage/Contents.swift
@@ -7,14 +7,72 @@ import Foundation
 
 code(for: "Namespace Debug print") {
     // Recreate the print function in an Enum namespace
+    enum Debug {
+        static func print(
+            _ items: Any...,
+            separator: String = " ",
+            terminator: String = "\n"
+        ) {
+            Swift.print(items, separator: separator, terminator: terminator)
+        }
+    }
+    Debug.print(1, 2, 3)
 }
 
 code(for: "Emoji previx") {
     // add a number of emooji cases to the enum and use as a prefix for the print
+    enum Debug: String {
+        case pin = "📌"
+        case star = "🌟"
+        case note = "📝"
+        case globe = "🌎"
+        static func print(
+            _ items: Any...,
+            type: Debug,
+            separator: String = " ",
+            terminator: String = "\n"
+        ) {
+            Swift.print(type.rawValue,
+                        items.map {"\($0)"}.joined(separator: separator),
+                        separator: separator,
+                        terminator: terminator)
+        }
+    }
+    Debug.print(1, 2, 3, type: .pin)
 }
 
 code(for: "Debug Identifiers") {
     // Extend the function to allow for an optional set of Swift Keyword debug identifiers
+    enum Debug: String {
+        case pin = "📌"
+        case star = "🌟"
+        case note = "📝"
+        case globe = "🌎"
+        static func print(
+            _ items: Any...,
+            type: Debug,
+            extended: Bool = false,
+            separator: String = " ",
+            terminator: String = "\n"
+        ) {
+            Swift.print(type.rawValue,
+                        items.map {"\($0)"}.joined(separator: separator),
+                        separator: separator,
+                        terminator: terminator)
+            if extended {
+                Swift.print(
+                """
+                \(#fileID)
+                function: \(#function)
+                line: \(#line)
+                ----------------------------------------
+                """
+                )
+            }
+        }
+    }
+    Debug.print(1,2,3, type: .note)
+    Debug.print(1,2,3, type: .note, extended: true)
 }
 /*:
  [< Previous](@previous)                    [Home](Introduction)
